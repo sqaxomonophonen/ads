de0182e033a5188215e172ec61b359861feefa4e
Anders Kaare Straadt <anders@straadt.dk>
2023-09-12 00:46:18 +0200

    pretty window!
    my my it's starting to enter "the helpful phase"?

--- hack.js
+++ hack.js
@@ -1,3 +1,6 @@
+$  = x => document.querySelector(x);
+MAKE = tag => document.createElement(tag);
+
 function unpack_array_object(xs) {
 	if (xs === null) return null;
 	let o = {};
@@ -8,7 +11,71 @@ function unpack_array_object(xs) {
 }
 
 function present(o) {
-	document.getElementById("debug").innerText = JSON.stringify(o);
+	$("#entrypoint").innerText = ":" + o.entrypoint_word + " (" + o.entrypoint_filename + ")";
+	$("#passes").innerText = o.n_passes[0] + "/" + o.n_passes[1];
+	let c = $("#stack");
+	c.innerHTML = "";
+
+	const table = MAKE("table");
+
+	const stack = o.stack;
+	const n = stack.length;
+	for (let i = (n-1); i >= 0; i--) {
+		const value = stack[i];
+
+		const tr = MAKE("tr");
+
+		const td0 = MAKE("td");
+		td0.innerText = "#" + (1+i);
+		tr.appendChild(td0);
+
+		const td1 = MAKE("td");
+		//td1.innerText = JSON.stringify(value);
+
+		function push(subcls, txt) {
+			const e = MAKE("span");
+			e.setAttribute("class", "tok-" + subcls);
+			e.innerText = txt;
+			td1.appendChild(e);
+		}
+
+		function render_array(v, subcls) {
+			const n = v.length;
+			push(subcls, "[");
+			for (let i = 0; i < n; i++) {
+				if (i > 0) push(subcls,",");
+				render_value(v[i]);
+			}
+			push(subcls, "]");
+		}
+
+		function render_value(v) {
+			if (typeof v === "number") {
+				push("number", ""+v);
+			} else if (typeof v === "boolean") {
+				push("boolean", ""+v);
+			} else if (!v) {
+				push("null", ""+v);
+			} else if (v instanceof Array) {
+				render_array(v, "array");
+			} else if (v instanceof Object) {
+				// TODO type specific renders?
+				push("tagged-prefix", v.t);
+				render_array(v.x, "tagged");
+			} else {
+				push("null", "?");
+			}
+		}
+		render_value(value);
+
+		tr.appendChild(td1);
+
+		table.appendChild(tr);
+	}
+	c.appendChild(table);
+
+
+	//document.getElementById("debug").innerText = JSON.stringify(o);
 }
 
 window.addEventListener("load", () => {
